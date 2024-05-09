61bc091d43cf81930503dd94afd38fa156f605cb
BLeo16 <blfloresa@utn.edu.ec>
2024-05-08 23:24:59 -0500

    listPage

--- MobileAppUsers/Pages/ListUsers.xaml.cs
+++ MobileAppUsers/Pages/ListUsers.xaml.cs
@@ -1,3 +1,6 @@
+using APIConsumer;
+using MobileAppUsers.Models;
+
 namespace MobileAppUsers.Pages;
 
 public partial class ListUsers : ContentPage
@@ -6,11 +9,40 @@ public partial class ListUsers : ContentPage
     public ListUsers()
 	{
 		InitializeComponent();
+
 	}
 
     private void btnBack_Clicked(object sender, EventArgs e)
     {
         Navigation.PopAsync();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        LoadUsers();
+    }
+
+    private async Task LoadUsers()
+    {
+        try
+        {
+            // Clear existing children
+            UsersStackLayout.Children.Clear();
+
+            var users = await Crud<User>.Read(ApiUrl);
+            foreach (var user in users)
+            {
+                var labelId = new Label { Text = user.id.ToString() };
+                var label = new Label { Text = user.username };
+                UsersStackLayout.Children.Add(labelId);
+                UsersStackLayout.Children.Add(label);
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
     }
 
     private void btnCreate_Clicked(object sender, EventArgs e)
